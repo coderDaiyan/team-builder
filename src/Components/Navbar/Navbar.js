74f85493a56c5fdb02b8d377a0b8919492c883b0
coderDaiyan <daiyanabdallah@gmail.com>
2021-03-01 22:25:08 +0600

    changed the title name of project

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,36 +4,10 @@ import "./navbar.css";
 const Navbar = () => {
   return (
     <>
-      {/* <nav className="navbar bg-dark">
-        <div className="container-fluid">
-          <a
-            className="navbar-brand h1"
-            style={{ color: "white" }}
-            href="#home"
-          >
-            <h2>Dream11</h2>
-          </a>
-          <div class="collapse d-flex navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <a
-                style={{ color: "white" }}
-                class="nav-link active"
-                aria-current="page"
-                href="#players"
-              >
-                Players
-              </a>
-              <a style={{ color: "white" }} class="nav-link" href="#footer">
-                Contact Me
-              </a>
-            </div>
-          </div>
-        </div>
-      </nav> */}
       <nav class="navbar navbar-dark bg-dark">
         <div class="container-fluid">
           <a href="#home" class="navbar-brand">
-            Dream 11
+            Team Builder - Make Your Favorite Team
           </a>
           <div className="navbar-dark d-flex">
             <a href="#players">
--- src/Components/TeamDetails/TeamDetails.js
+++ src/Components/TeamDetails/TeamDetails.js
@@ -3,17 +3,12 @@ import "./team-detail.css";
 
 const TeamDetails = (props) => {
   const fullTeam = props.fullTeam;
-  // let totalBudget = 0;
-  // for (let i = 0; i < fullTeam.length; i++) {
-  //   const player = fullTeam[i];
-  //   totalBudget = totalBudget + player.salary;
-  // }
   const totalBudget = fullTeam.reduce(
     (total, player) => total + player.salary,
     0
   );
   return (
-    <div id="home" className="team-detail">
+    <div className="team-detail">
       <h2>Total Player Selected: {props.playerLength}</h2>
       <h3>Total Budget: {totalBudget}€</h3>
       <h3>All Players:</h3>
